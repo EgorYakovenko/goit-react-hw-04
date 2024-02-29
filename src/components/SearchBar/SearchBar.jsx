87894e55eaef6206d00005800399961fb4bb815c
EgorYakovenko <Egor.Yakovenko@ukr.net>
2024-03-01 00:50:07 +0100

    qwe

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,3 +1,17 @@
-function SearchBar() {}
+function SearchBar() {
+  return (
+    <header>
+      <form>
+        <input
+          type="text"
+          autocomplete="off"
+          autofocus
+          placeholder="Search images and photos"
+        />
+        <button type="submit">Search</button>
+      </form>
+    </header>
+  );
+}
 
 export default SearchBar;
